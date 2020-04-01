7a63cc92007466fda0762a1d103bbb1edfc6006d
Ravi Prakash Singh <Ravip@cloudthing.com>
2020-04-01 10:49:51 +0530

    Updates

---
+++ precache-manifest.07606a7617d2a67000b14ea4cb28870c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ae3bfd337983f90761f4897017f10c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f14749fff0f4ccf8f980",
+    "url": "/static/css/main.8f8bdae7.chunk.css"
+  },
+  {
+    "revision": "f20c2a2b630fcbb13aa8",
+    "url": "/static/js/2.eeeba9b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eeeba9b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f14749fff0f4ccf8f980",
+    "url": "/static/js/main.64005671.chunk.js"
+  },
+  {
+    "revision": "222e6851b2ddb632083c",
+    "url": "/static/js/runtime-main.79dff054.js"
+  }
+]);
